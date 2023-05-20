ab3d9b5c5656e2db6b8b4c7590b4f22c19420c3f
psremnev <52470169+psremnev@users.noreply.github.com>
2023-05-20 22:37:02 +0300

    +

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,26 +1,19 @@
 import { default as devConfig } from './webpack.dev.js';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 
-const [server, client] = devConfig;
-
 const config = {
   mode: 'production',
+  watch: false,
   externals: {
     express: 'express',
   },
   optimization: {
     minimize: true,
-    minimizer: [new CssMinimizerPlugin()],
-  },
+    minimizer: [new CssMinimizerPlugin()]
+  }
 };
 
-export default [
-  {
-    ...config,
-    ...server,
-  },
-  {
-    ...config,
-    ...client,
-  },
-];
+export default {
+  ...devConfig,
+  ...config
+};
