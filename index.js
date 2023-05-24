d515fc079e5749d110d758dbe7d76d6d7c821491
psremnev <52470169+psremnev@users.noreply.github.com>
2023-05-24 23:22:44 +0300

    +

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 addEventListener('DOMContentLoaded', (event) => {});
+import { render } from 'react-dom';
+import Demo from 'src/client_modules/Demo/Demo';
+
 addEventListener('DOMContentLoaded', (event) => {
-    document.querySelector('.root').innerHTML = 'test';
+    render(<Demo />, document.querySelector('.root'));
 });
