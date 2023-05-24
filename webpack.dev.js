e005cfbed968253b36dff73594141433dbd7a867
psremnev <52470169+psremnev@users.noreply.github.com>
2023-05-25 00:15:31 +0300

    +

--- webpack.dev.js
+++ webpack.dev.js
@@ -13,7 +13,7 @@ import {
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+console.log('__dirname', __dirname);
 const resolveCfg = {
   extensions: ['.js', '.jsx', '.css'],
   // TsconfigPathsPlugin - чтобы работали пути импорта из tsconfig
