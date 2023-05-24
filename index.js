536dbb728df8bab8d71fc41741352eda69ff9608
psremnev <52470169+psremnev@users.noreply.github.com>
2023-05-25 01:14:41 +0300

    +

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 addEventListener('DOMContentLoaded', (event) => {});
 // <!-- Git Hub Site -->
-import { createRoot } from 'react-dom';
-import { createElement } from 'react';
-import Demo from 'client_modules/Demo/Demo';
+import Demo from './client_modules/Demo/Demo';
 
-const root = createRoot(document.querySelector('.root'));
-root.render(createElement(Demo));
+const root = ReactDOM.createRoot(document.querySelector('.root'));
+root.render(React.createElement(Demo));
