73be75195321ccb7d7c48c7847c7ebf49c4a7be3
psremnev <52470169+psremnev@users.noreply.github.com>
2023-05-25 02:20:43 +0300

    +

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,6 @@
 // <!-- Git Hub Site -->
+import {default as Demo} from './demo';
+
 window.addEventListener('DOMContentLoaded', () => {
   const root = ReactDOM.createRoot(document.querySelector('.root'));
   root.render(React.createElement(Demo));
