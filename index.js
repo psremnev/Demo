ddf5df43aaca3376c813dc79a6edf03734e6eb92
psremnev <52470169+psremnev@users.noreply.github.com>
2023-05-25 00:05:59 +0300

    +

--- index.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-addEventListener('DOMContentLoaded', (event) => { });
-// <!-- Git Hub Site -->
-import { render } from 'react-dom';
-import Demo from 'src/client_modules/Demo/Demo';
-addEventListener('DOMContentLoaded', (event) => {
-    render(_jsx(Demo, {}), document.querySelector('.root'));
-});
