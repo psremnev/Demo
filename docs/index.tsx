b40bb9f0eb09a405b8672c591fa93d56a8d6117f
psremnev <52470169+psremnev@users.noreply.github.com>
2023-05-25 03:12:49 +0300

    +

--- docs/index.tsx
+++ docs/index.tsx
@@ -1,7 +1,7 @@
 // <!-- Git Hub Site -->
 import { default as Demo } from 'client_modules/Demo/Demo';
 import { createRoot } from 'react-dom';
-import 'index.scss';
+import './index.scss';
 
 window.addEventListener('DOMContentLoaded', () => {
   const root = createRoot(document.querySelector('.root'));
