9056b6c1fee453b4adb1ee9023bcf68677ff5cb6
psremnev <52470169+psremnev@users.noreply.github.com>
2023-05-25 03:35:24 +0300

    +

--- docs/index.tsx
+++ docs/index.tsx
@@ -3,7 +3,16 @@ import { default as Demo } from 'client_modules/Demo/Demo';
 import { createRoot } from 'react-dom';
 import './index.scss';
 
+const DemoPage = () => {
+  return (
+    <>
+      <span style={{ width: '100%', textAlign: 'center' }}>Demo Page</span>
+      <Demo />
+    </>
+  );
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   const root = createRoot(document.querySelector('.root'));
-  root.render(<Demo preloadData={ {} }/>, root);
+  root.render(<DemoPage />, root);
 });
